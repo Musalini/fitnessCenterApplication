ad71fca1568b3f0541c857913db75f768e08a924
Musa Muradzade <muradzade.musa@gmail.com>
2022-01-25 18:03:05 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main implements Calculator {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/company/Member.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Member {
+    char memberType;
+    int memberId;
+    String name;
+    double fees;
+
+    public char getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(char memberType) {
+        this.memberType = memberType;
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getFees() {
+        return fees;
+    }
+
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    public Member(char memberType, int memberId, String name, double fees) {
+        this.memberType = memberType;
+        this.memberId = memberId;
+        this.name = name;
+        this.fees = fees;
+    }
+}
